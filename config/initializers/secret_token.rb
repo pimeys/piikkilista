8a6fb83c899781db23aadcb9a316442e143bbb61
Julius de Bruijn <julius.bruijn@sponsorpay.com>
2012-11-07 21:09:19 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piikki::Application.config.secret_token = '074acdd9f794458ea0d1bbb8ed8e7d4921f656778c3609910015e218905a678c7d28e49e6b9ea269cc0197d7333ef8a41fd9fd974f73530ee0a6010aba82dc62'
